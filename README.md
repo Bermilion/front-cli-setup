ef83024114b9d8782dcee55c7a4834692b273bbd
volkov.i <volkov.i@studio2i.ru>
2019-04-19 13:33:53 +0300

    начал писать установочник

---
+++ README.md
@@ -0,0 +1,8 @@
+##Installation
+
+```sh
+curl -o- https://raw.githubusercontent.com/Bermilion/front-cli/master/setup.py | python
+```
+```sh
+wget -qO- https://raw.githubusercontent.com/Bermilion/front-cli/master/setup.py | python
+```
---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+env = os.environ
+downloader = 'curl' if os.path.exists('/usr/bin/curl') else 'wget'
+directory = env['HOME'] + '/.front-cli'
+
+if os.path.exists(directory) != 'true':
+    os.mkdir(directory)
+    os.chdir(directory)
+    print (os.getcwd())
+    # os.system('git clone ')
+
+if os.path.exists('/usr/local/bin/pip'):
+    os.system('pip -V')
+else:
+    if downloader == 'curl':
+        os.system(downloader + ' https://bootstrap.pypa.io/get-pip.py -o get-pip.py | python')
+    else:
+        os.system(downloader + ' https://bootstrap.pypa.io/get-pip.py -O get-pip.py | python')
+
+    os.system('pip install -U pip')
+    os.system('pip -V')
+
